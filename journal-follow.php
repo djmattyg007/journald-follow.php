8528ccfd43709e9992e83a6380b88ab69805b019
djmattyg007 <git@matthewgamble.net>
2014-05-12 19:37:14 +1000

    Fixed lots of code style problems.

--- journal-follow.php
+++ journal-follow.php
@@ -3,38 +3,30 @@
  * Released under WTFPL v2 <http://sam.zoy.org/wtfpl/>
  */
 
-/* Iterator extends Traversable {
-	void    rewind()
-	boolean valid()
-	void    next()
-	mixed   current()
-	scalar  key()
-}
-calls:	rewind, valid==true, current, key
-	next, valid==true, current, key
-	next, valid==false
-*/
+class Journal implements Iterator
+{
+	protected $filter;
+	protected $startpos;
+	protected $proc;
+	protected $stdout;
+	protected $entry;
 
-class Journal implements Iterator {
-	private $filter;
-	private $startpos;
-	private $proc;
-	private $stdout;
-	private $entry;
-
-	static function _join_argv($argv) {
+	public static function _join_argv($argv)
+	{
 		return implode(" ",
 			array_map(function($a) {
 				return strlen($a) ? escapeshellarg($a) : "''";
 			}, $argv));
 	}
 
-	function __construct($filter=[], $cursor=null) {
+	public function __construct($filter = [], $cursor = null)
+	{
 		$this->filter = $filter;
 		$this->startpos = $cursor;
 	}
 
-	function _close_journal() {
+	protected function _close_journal()
+	{
 		if ($this->stdout) {
 			fclose($this->stdout);
 			$this->stdout = null;
@@ -46,9 +38,11 @@ class Journal implements Iterator {
 		$this->entry = null;
 	}
 
-	function _open_journal($filter=[], $cursor=null) {
-		if ($this->proc)
+	protected function _open_journal($filter = [], $cursor = null)
+	{
+		if ($this->proc) {
 			$this->_close_journal();
+		}
 
 		$this->filter = $filter;
 		$this->startpos = $cursor;
@@ -61,48 +55,58 @@ class Journal implements Iterator {
 		$cmd = array_merge($cmd, $filter);
 		$cmd = self::_join_argv($cmd);
 
-		$fdspec = [
-			0 => ["file", "/dev/null", "r"],
-			1 => ["pipe", "w"],
-			2 => ["file", "/dev/null", "w"],
-		];
+		$fdspec = array(
+			0 => array("file", "/dev/null", "r"),
+			1 => array("pipe", "w"),
+			2 => array("file", "/dev/null", "w"),
+		);
 
 		$this->proc = proc_open($cmd, $fdspec, $fds);
-		if (!$this->proc)
+		if (!$this->proc) {
 			return false;
+		}
 		$this->stdout = $fds[1];
 	}
 
-	function seek($cursor) {
+	public function seek($cursor)
+	{
 		$this->_open_journal($this->filter, $cursor);
 	}
 
-	function rewind() {
+	public function rewind()
+	{
 		$this->seek($this->startpos);
 	}
 
-	function next() {
+	public function next()
+	{
 		$line = fgets($this->stdout);
-		if ($line === false)
+		if ($line === false) {
 			$this->entry = false;
-		else
+		} else {
 			$this->entry = json_decode($line);
+		}
 	}
 
-	function valid() {
-		return ($this->entry !== false);
+	function valid()
+	{
 		/* null is valid, it just means next() hasn't been called yet */
+		return ($this->entry !== false);
 	}
 
-	function current() {
-		if (!$this->entry)
+	function current()
+	{
+		if (!$this->entry) {
 			$this->next();
+		}
 		return $this->entry;
 	}
 
-	function key() {
-		if (!$this->entry)
+	function key()
+	{
+		if (!$this->entry) {
 			$this->next();
+		}
 		return $this->entry->__CURSOR;
 	}
 }
@@ -113,6 +117,8 @@ foreach ($a as $cursor => $item) {
 	echo "================\n";
 	var_dump($cursor);
 	//print_r($item);
-	if ($item)
+	if ($item) {
 		var_dump($item->MESSAGE);
+	}
 }
+
